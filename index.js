0053ada62835cd08d4ba9fb70ed79666eed54a74
Kamran Hussein <chinterswill@gmail.com>
2022-10-31 12:51:35 -0400

    commit tree ab30a414be5e106353778ef429a681e992a99220

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2);
+console.log(multiply);
+
+const random = Math.ceil(Math.random() * 10); 
+
+
+const num3 = 16
+const num4 = 12
+const mod = (num3 % num4);
+
+const max = Math.max(1, 5, 8, 20);
